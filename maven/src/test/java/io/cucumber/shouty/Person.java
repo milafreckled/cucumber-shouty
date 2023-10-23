953eedf8763f12abc69a171fd32765c4c9b936eb
milafreckled <miladul2014@gmail.com>
2023-10-23 14:01:23 +0200

    complete step definitions

---
+++ maven/src/test/java/io/cucumber/shouty/Person.java
@@ -0,0 +1,13 @@
+package io.cucumber.shouty;
+
+public class Person {
+    public void moveTo(int distance){
+
+    }
+    public void shout(String message){
+
+    }
+    public void getMessageHeard(){
+
+    }
+}
--- maven/src/test/java/io/cucumber/shouty/StepDefinitions.java
+++ maven/src/test/java/io/cucumber/shouty/StepDefinitions.java
@@ -3,19 +3,30 @@ package io.cucumber.shouty;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert.*;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
 
 public class StepDefinitions {
+    private Person lucy;
+    private Person sean;
     @Given("Lucy is located {int} meters from Sean")
-    public void lucyIsLocatedMetersFromSean(int arg0) {
+    public void lucyIsLocatedMetersFromSean(int distance) {
+        lucy = new Person();
+        sean = new Person();
+        lucy.moveTo(distance);
+        System.out.println("distance = " + distance);
     }
 
     @When("Sean shouts {string}")
-    public void seanShouts(String arg0) throws Throwable {
+    public void seanShouts(String message) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        sean.shout(message);
     }
 
     @Then("Lucy hears Sean's message")
     public void lucyHearsSeanSMessage() {
+        assertEquals(asList(messageFromSean), lucy.getMessageHeard());
     }
 }
