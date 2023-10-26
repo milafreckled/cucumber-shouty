a2be0cae186edb3391da7e93e6a1825510342e01
milafreckled <miladul2014@gmail.com>
2023-10-26 15:42:22 +0200

    complete step definitions

--- maven/src/test/java/io/cucumber/shouty/Person.java
+++ maven/src/test/java/io/cucumber/shouty/Person.java
@@ -1,13 +1,19 @@
 package io.cucumber.shouty;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
+    public static List<String> messages = new ArrayList<String>();
     public void moveTo(int distance){
 
     }
     public void shout(String message){
-
+            messages.add(message);
     }
-    public void getMessageHeard(){
-
+    public List<String> getMessagesHeard(){
+//        List<String> result = new ArrayList<String>();
+//        result.add("free bagels at Sean's");
+        return messages.subList(messages.size()-1, messages.size());
     }
 }
--- maven/src/test/java/io/cucumber/shouty/StepDefinitions.java
+++ maven/src/test/java/io/cucumber/shouty/StepDefinitions.java
@@ -3,7 +3,6 @@ package io.cucumber.shouty;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.Assert.*;
 
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
@@ -11,6 +10,7 @@ import static org.junit.Assert.assertEquals;
 public class StepDefinitions {
     private Person lucy;
     private Person sean;
+    private String messageFromSean;
     @Given("Lucy is located {int} meters from Sean")
     public void lucyIsLocatedMetersFromSean(int distance) {
         lucy = new Person();
@@ -23,10 +23,11 @@ public class StepDefinitions {
     public void seanShouts(String message) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         sean.shout(message);
+        messageFromSean = message;
     }
 
     @Then("Lucy hears Sean's message")
     public void lucyHearsSeanSMessage() {
-        assertEquals(asList(messageFromSean), lucy.getMessageHeard());
+        assertEquals(asList(messageFromSean), lucy.getMessagesHeard());
     }
 }
